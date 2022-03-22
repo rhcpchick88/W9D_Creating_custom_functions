eabe05ce38678722146393b189dbfd11896709f9
samantha <langridge.se@gmail.com>
2022-03-21 20:05:54 -0600

    attempted startsWith

--- js/app.js
+++ js/app.js
@@ -7,16 +7,27 @@ function mathematics(integer1,integer2){
 var integer1=8;
 var integer2=50;
 
-mathematics(integer1,integer2); //since it automatially prints the result I just have to state the function.
+mathematics(integer1,integer2); 
 
-function truefalse(str){
-    if (str.length>10){
-        console.log('This string is more than 10 characters');
+//named my function 
+function length(str1){
+    if (str1.length>10){    //used the argument str.length to measure the length of the string
+        console.log('This string is more than 10 characters'); //true statement
     } else{
-        console.log('This string is 10 characters or less')
+        console.log('This string is 10 characters or less') //false statement
     }
 }
 
-var str='Hallo';
+var str1='Hallo'; //tested my string lengths and the function itself.
 
-truefalse(str);
+length(str1);
+
+// this one I couldn't get working for some reason 
+
+function startsWith(str2){
+    result = str2.toString(str2).startsWith('ph'); //tried filtering out words starting with ph and then logging the result
+    console.log(result);
+}
+
+var str2 = ['street','tree','photo','lamp','phone'] //created my array
+startsWith(str2); //prints out false not sure why
