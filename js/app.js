464d0c5ac19e2b4a17ca180c124e7284e84b0038
samantha <langridge.se@gmail.com>
2022-03-21 19:02:07 -0600

    finished function1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//named my function mathematics and stated it involves my two variables, integer1 and integer2.
+function mathematics(integer1,integer2){
+    console.log(((integer1+integer2)*5)); // used BEDMAS to make the function add the two arguments together and multiply by 5.
+}
+//created my two arguments, this works with any numbers as I have tested it multiple times.
+
+var integer1=8;
+var integer2=50;
+
+mathematics(integer1,integer2); //since it automatially prints the result I just have to state the function.
+
+
